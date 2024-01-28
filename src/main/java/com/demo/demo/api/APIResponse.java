64b6aa3e426c567523642a0f1ad9fc58c958d12c
Hariniaina1764! <grace.andrianisoa@gmail.com>
2024-01-28 20:47:33 +0100

    crud annonce + voiture non vendues

---
+++ src/main/java/com/demo/demo/api/APIResponse.java
@@ -0,0 +1,30 @@
+package com.demo.demo.api;
+
+public class APIResponse {
+    String error;
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    Object data;
+
+    
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+    
+    public APIResponse(String error, Object data) {
+        this.error = error;
+        this.data = data;
+    }
+}
---
+++ src/main/java/com/demo/demo/vehicule/repository/annonce/AnnonceRepository.java
@@ -0,0 +1,14 @@
+package com.demo.demo.vehicule.repository.annonce;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.demo.demo.vehicule.model.annonce.Annonce;
+
+public interface AnnonceRepository extends MongoRepository<Annonce, String> {
+    List<Annonce> findByStatusAndEstValide(int status, int estValide);
+    List<Annonce> findByIdproprietaire(int idproprietaire);
+    Optional<Annonce> findById(String id);
+}
